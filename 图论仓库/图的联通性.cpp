5c37a78f912f8ee4070c16e8b2559fd12a3b4197
qiyue7 <zhangboqi007@live.com>
2019-02-26 01:17:21 +0800

    0

--- 图论仓库/图的联通性.cpp
+++ 图论仓库/图的联通性.cpp
@@ -22,70 +22,89 @@ using namespace std;
 
 namespace strong
 {
-	    const int MAXN = 10010;
-	    const int MAXM = 501000;
-		class Edge
+	const int MAXN = 500010;
+	const int MAXM = 1001000;
+	class Edge
+	{
+	public:
+		int to, next, w;
+	};
+	Edge edge[MAXM];
+	int head[MAXN], tot;
+	int Low[MAXN], DFN[MAXN], Stack[MAXN], Belong[MAXN];
+	int Index, top;
+	int scc;
+	bool Instack[MAXN];
+	int num[MAXN];
+	vector<pair<int, int> > edge2[MAXN];
+	void addedge(int u, int v, int w)
+	{
+		edge[tot].to = v;
+		edge[tot].next = head[u];
+		edge[tot].w = w;
+		head[u] = tot++;
+	}
+	void tarjan(int u)
+	{
+		int v;
+		Low[u] = DFN[u] = ++Index;
+		Stack[top++] = u;
+		Instack[u] = true;
+		for (int i = head[u]; i != -1; i = edge[i].next)
 		{
-		public:
-			int to, next;
-		};
-		Edge egde[MAXM];
-		int head[MAXN], tot;
-		int Low[MAXN], DFN[MAXN], Stack[MAXN], Belong[MAXN];
-		int Index, top;
-		int scc;
-		bool Instack[MAXN];
-		int num[MAXN];
-		void addedge(int u, int v)
-		{
-			egde[tot].to = v;
-			egde[tot].next = head[u];
-			head[u] = tot++;
-		}
-		void tarjan(int u)
-		{
-			int v;
-			Low[u] = DFN[u] = ++Index;
-			Stack[top++] = u;
-			Instack[u] = true;
-			for (int i = head[u]; i != -1; i = egde[i].next)
+			v = edge[i].to;
+			if (DFN[v] == 0)
 			{
-				v = egde[i].to;
-				if (DFN[v] == 0)
+				tarjan(v);
+				if (Low[u] > Low[v])
+					Low[u] = Low[v];
+			}
+			else if (Instack[v] && Low[u] > DFN[v])
+				Low[u] = DFN[v];
+		}
+		if (Low[u] == DFN[u])
+		{
+			scc++;
+			do {
+				v = Stack[--top];
+				Instack[v] = false;
+				Belong[v] = scc;
+				num[scc]++;
+			} while (v != u);
+		}
+	}
+	void solve(int N)
+	{
+		memset(DFN, 0, sizeof(DFN));
+		memset(Instack, false, sizeof(Instack));
+		memset(num, 0, sizeof(num));
+		Index = scc = top = 0;
+		for (int i = 1; i <= N; ++i)
+			if (DFN[i] == 0)
+				tarjan(i);
+	}
+	int ans[MAXN];
+	void rebuild(int N)
+	{
+		for (int i = 1; i <= N; ++i)
+			for (int j = head[i]; j != -1; j = edge[j].next)
+			{
+				if (Belong[i] != Belong[edge[j].to])
 				{
-					tarjan(v);
-					if (Low[u] > Low[v])
-						Low[u] = Low[v];
+					edge2[Belong[i]].push_back(make_pair(Belong[edge[j].to], edge[j].w));
 				}
-				else if (Instack[v] && Low[u] > DFN[v])
-					Low[u] = DFN[v];
+				else
+				{
+					if (edge[i].w == 1)
+						ans[Belong[i]] = 1;
+				}
 			}
-			if (Low[u] == DFN[u])
-			{
-				scc++;
-				do {
-					v = Stack[--top];
-					Instack[v] = false;
-					Belong[v] = scc;
-					num[scc]++;
-				} while (v != u);
-			}
-		}
-		void solve(int N)
-		{
-			memset(DFN, 0, sizeof(DFN));
-			memset(Instack, false, sizeof(Instack));
-			memset(num, 0, sizeof(num));
-			Index = scc = top = 0;
-			for (int i = 1; i <= N; ++i)
-				if (DFN[i] == 0)
-					tarjan(i);
-		}
-		void init()
-		{
-			tot = 0;
-			memset(head, -1, sizeof(head));
-		}
+	}
+	void init()
+	{
+		tot = 0;
+		memset(head, -1, sizeof(head));
+	}
 }
 
 namespace bridge2
